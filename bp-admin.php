a4c2474e7ff14a8524cacf7424b12ad9d7470446
Riccardo Silvi <silvicardo@gmail.com>
2019-02-01 16:05:14 +0100

    admin page placeholder text

--- bp-admin.php
+++ bp-admin.php
@@ -1,5 +1,8 @@
 <?php include 'header.php'; ?>
 
-    <h1>We are still building this app</h1>
+  <div class="container jumbotron">
+    <h1>We are still building this page</h1>
+  </div>
+
 
 <?php include 'footer.php'; ?>
